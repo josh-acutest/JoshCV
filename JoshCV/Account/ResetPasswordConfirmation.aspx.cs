dc1438ff60c411124b7e6707c0ade6092457147c
josh-acutest <josh.crossley@acutesales.co.uk>
2016-11-29 19:49:42 +0000

    create

---
+++ JoshCV/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace JoshCV.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ JoshCV/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JoshCV.Startup))]
+namespace JoshCV
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
