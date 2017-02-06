7a4d59db7204493fb55ca6897408a7c0fc560954
josh-acutest <josh.crossley@acutesales.co.uk>
2017-02-06 14:01:17 +0000

    j

--- JoshCV/Scripts/ToDo3.js
+++ JoshCV/Scripts/ToDo3.js
@@ -21,25 +21,62 @@ var arrayTasks = [taskTest1, taskTest2, taskTest3, taskTest4, taskTest5];
 
 
 function loadData() {
-    
-    clearDom();
+   
+    var inptVal = document.getElementById("_inputTitle").value;
 
-    var list = document.getElementById("inputResult");
+    if (inptVal != "") {
 
-    for (i = 0; i < arrayTasks.length; i++) {
+        clearDom();
 
-        //get id of instance
-        var taskId = "the ID: " + arrayTasks[i].id;
-        var taskTitle = arrayTasks[i].title;
-        
-        var taskandID = taskId + " " + taskTitle;
-        var textNode = document.createTextNode(taskandID);
-        var listItem = document.createElement("li");
-        listItem.appendChild(textNode);
-        list.appendChild(listItem);
-        //event added to items
-        listItem.onclick = removeItem;
+        var list = document.getElementById("inputResult");
+
+        var dataAdd = 0;
+
+        for (i = 0; i < arrayTasks.length; i++) {
+
+            //get id of instance
+            var taskId = "the ID: " + arrayTasks[i].id;
+            var taskTitle = arrayTasks[i].title;
+
+            var taskandID = taskId + " " + taskTitle;
+            var textNode = document.createTextNode(taskandID);
+            var listItem = document.createElement("li");
+           
+           
+            
+            // need to figure how to set attribute and append by 1 for each array item, then use this id to splice from array
+            //listItem.setAttribute();
+
+
+            listItem.appendChild(textNode);
+            list.appendChild(listItem);
+            
+            console.log(listItem.attributes);
+
+            //event added to items
+            //access the parameters and attributes of a html element - li. by using e.target
+            //this gets the e represents the item clicked, so li. then targets opens the attributes
+
+
+            listItem.onclick = function (e) {
+
+                console.log(e);
+
+                //var liText = e.target.innerText;
+                //console.log(liText);
+                //var tryThis = arrayTasks.valueOf(liText);
+                //arrayTasks.splice(tryThis, 1);
+
+                
+
+            }
+        }
     }
+
+    else {
+        alert("enter a value");
+    }
+
 }
 
 function clearDom() {
@@ -76,5 +113,6 @@ function removeItem(e) {
 
 //Next task to create remove function
 
+// data set attribute
 
 //then edit function
