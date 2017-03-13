86298b7b00ac571d49c090045b5de96c650f59b4
josh-acutest <josh.crossley@acutesales.co.uk>
2017-03-13 22:37:41 +0000

    jc
    jc

---
+++ JoshCV/CarGame.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CarGame.aspx.cs" Inherits="JoshCV.CarGame" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link href="Content/bootstrap.css" rel="stylesheet" />
+    
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div class="container race-container">
+        <div class="row">
+            <div class="col-md-12">
+                <ul id="car-list">
+
+                </ul>
+            </div>
+        </div>
+        <a href="#" class="btn btn-danger" onclick="startRace()">Start Race</a>
+    
+    </div>
+
+
+    </form>
+    <script src="Scripts/CarGame.js"></script>
+</body>
+</html>
---
+++ JoshCV/Scripts/CarGame.js
@@ -0,0 +1,38 @@
+﻿function car(id, name) {
+    this.Id = id;
+    this.Name = name;
+}
+
+var arrCars = new Array();
+
+var car1 = new car(1, "Honda Civic");
+var car2 = new car(2, "Volkswagen Golf");
+var car3 = new car(3, "Vauxhall Corsa");
+
+arrCars = [car1, car2, car3];
+carList = document.getElementById("car-list");
+
+function generateCars() {
+    
+
+    h3Tag = document.createElement("h3");
+    h3Wording = document.createTextNode("Car game");
+    h3Tag.appendChild(h3Wording);
+    carList.appendChild(h3Tag);
+
+    for (i = 0; i < arrCars.length; i++) {
+        
+        var name = arrCars[i].Name;
+        var carName = document.createTextNode(name);
+        var listItem = document.createElement("li")
+        listItem.appendChild(carName);
+        
+        carList.appendChild(listItem);
+    }
+}
+
+generateCars();
+
+function startRace() {
+    carList.
+}
